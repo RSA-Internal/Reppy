db3b714aaacb608f0e45a4c769f47db762b2420d
Chris <chris.barwick16@gmail.com>
2021-08-20 00:34:56 -0700

    Implement GuildDataDAO and Tests

--- src/daos/GuildDataDao.ts
+++ src/daos/GuildDataDao.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-mixed-spaces-and-tabs */
 /**
 	Required Methods
 
@@ -6,128 +7,338 @@
 	- Update Data
  */
 
+import type { Snowflake } from "discord.js";
+import guildDataModel, { IChannelData, IGuildData, IUserData } from "../models/guildData.model";
+
+export interface IDAOResult {
+	readonly result: boolean;
+	readonly message: string;
+	readonly guildData?: IGuildData;
+	readonly userData?: IUserData;
+}
+
 /**
- * Testing data...
- *
- * if (message.content === "testDBSetup") {
-			const channelId = message.channelId;
-
-			guildDataModel
-				.create({
-					guildId: message.guildId,
-					validChannels: [channelId],
-					userData: [
-						{
-							userId: "142090816150568960",
-							reputation: [
-								{
-									channelId: channelId,
-									reputation: 24,
-								},
-								{
-									channelId: "871596399773503528",
-									reputation: 12,
-								},
-							],
-						},
-						{
-							userId: "169208961533345792",
-							reputation: [
-								{
-									channelId: channelId,
-									reputation: 10,
-								},
-								{
-									channelId: "871596399773503528",
-									reputation: 20,
-								},
-							],
-						},
-					],
-				})
-				.catch(console.warn.bind(console));
-		} else if (message.content === "fetchTestData") {
-			guildDataModel
-				.find({ guildId: message.guildId! })
-				.then(guildData => {
-					if (guildData) {
-						const userData = guildData[0].userData;
-
-						if (userData) {
-							const userSpecific = userData.find(objs => objs.userId === message.author.id);
-
-							if (userSpecific) {
-								const reputation = userSpecific.reputation
-									.map(channelData => `${channelData.channelId}: ${channelData.reputation}`)
-									.join("\n");
-
-								if (reputation.length > 0) {
-									message.reply(reputation).catch(console.warn.bind(console));
-								} else {
-									message.reply("You do not have any test data.").catch(console.warn.bind(console));
-								}
-							} else {
-								message.reply("You do not have any test data.").catch(console.warn.bind(console));
+ * createGuildData is intended to be an intermediary between bot
+ * and MongoDB. This will reduce the amount of DB constructive
+ * calls outside of DB related locations.
+ */
+export async function createGuildData(guildId: Snowflake, validChannels?: string[]): Promise<IDAOResult> {
+	return new Promise<IDAOResult>(resolve => {
+		guildDataModel
+			.findOne({ guildId })
+			.then(guildData => {
+				if (guildData) {
+					resolve({ result: false, message: "guildData already exists.", guildData });
+				} else {
+					guildDataModel
+						.create({
+							guildId,
+							validChannels: validChannels ?? [],
+						})
+						.then(guildData => {
+							if (guildData) {
+								resolve({
+									result: true,
+									message: "Successfully created guildData.",
+									guildData,
+								});
 							}
-						} else {
-							message.reply("No data to fetch.").catch(console.warn.bind(console));
-						}
+						})
+						.catch((err: Error) => resolve({ result: false, message: err.message }));
+				}
+			})
+			.catch((err: Error) => resolve({ result: false, message: err.message }));
+	});
+}
+
+/**
+ * createUserData is intended to be an intermediary between bot
+ * and MongoDB. This will reduce the amount of DB constructive
+ * calls outside of DB related locations.
+ */
+export async function createUserData(
+	guildId: Snowflake,
+	userId: Snowflake,
+	channelId?: Snowflake,
+	reputation = 0
+): Promise<IDAOResult> {
+	return new Promise<IDAOResult>(resolve => {
+		guildDataModel
+			.findOne({ guildId })
+			.then(guildData => {
+				// TODO - Create guildData if does not exist.
+
+				if (guildData) {
+					const userData = guildData.userData;
+					const guildValidChannels = guildData.validChannels;
+
+					const userHasData = userData.find(userDataObject => userDataObject.userId === userId);
+					let isValidChannel = false;
+
+					if (channelId) {
+						isValidChannel = guildValidChannels.includes(channelId);
 					}
-				})
-				.catch(console.warn.bind(console));
-		}
- */
 
-/**
- * createGuildData is inteded to be an intermediary between bot
- * and MongoDB. This will reduce the amount of DB constructive
- * calls outside of DB related locations.
- */
-export function createGuildData(): void {
-	console.log("NYI - Create Guild Data");
+					if (!userHasData) {
+						// create data
+						const reputationData: IChannelData[] = [];
+
+						if (channelId && isValidChannel) {
+							reputationData.push({ channelId, reputation });
+						}
+
+						const userDataBase: IUserData = {
+							userId,
+							reputation: reputationData,
+						};
+
+						// update guild data in db
+						userData.push(userDataBase);
+
+						guildDataModel
+							.updateOne({ guildId }, { userData })
+							.then(() => {
+								resolve({
+									result: true,
+									message: "Successfully created userData.",
+									guildData,
+									userData: userDataBase,
+								});
+							})
+							.catch((err: Error) => resolve({ result: false, message: err.message, guildData }));
+					} else {
+						resolve({
+							result: false,
+							message: "userData already exists.",
+							guildData,
+							userData: userHasData,
+						});
+					}
+				} else {
+					resolve({ result: false, message: "Failed to fetch guildData." });
+				}
+			})
+			.catch((err: Error) => resolve({ result: false, message: err.message }));
+	});
 }
 
 /**
- * createUserData is inteded to be an intermediary between bot
+ * fetchGuildData is intended to be an intermediary between bot
  * and MongoDB. This will reduce the amount of DB constructive
  * calls outside of DB related locations.
  */
-export function createUserData(): void {
-	console.log("NYI - Create Guild Data");
+export async function fetchGuildData(guildId: Snowflake): Promise<IDAOResult> {
+	return new Promise<IDAOResult>(resolve => {
+		guildDataModel
+			.findOne({ guildId })
+			.then(guildData => {
+				if (guildData) {
+					resolve({ result: true, message: "Successfully fetched guildData.", guildData });
+				} else {
+					resolve({ result: false, message: "No guildData exists." });
+				}
+			})
+			.catch((err: Error) => resolve({ result: false, message: err.message }));
+	});
 }
 
 /**
- * fetchGuildData is inteded to be an intermediary between bot
+ * fetchUserData is intended to be an intermediary between bot
  * and MongoDB. This will reduce the amount of DB constructive
  * calls outside of DB related locations.
  */
-export function fetchGuildData(): void {
-	console.log("NYI - Fetch Guild Data");
+export async function fetchUserData(guildId: Snowflake, userId: Snowflake): Promise<IDAOResult> {
+	return new Promise<IDAOResult>(resolve => {
+		guildDataModel
+			.findOne({ guildId })
+			.then(guildData => {
+				if (guildData) {
+					const userData = guildData.userData.find(userDataObject => userDataObject.userId === userId);
+
+					if (userData) {
+						resolve({
+							result: true,
+							message: "Successfully fetched userData.",
+							guildData,
+							userData,
+						});
+					} else {
+						resolve({ result: false, message: "No userData exists.", guildData });
+					}
+				} else {
+					resolve({ result: false, message: "No guildData exists." });
+				}
+			})
+			.catch((err: Error) => resolve({ result: false, message: err.message }));
+	});
 }
 
 /**
- * fetchUserData is inteded to be an intermediary between bot
+ * updateGuildData is intended to be an intermediary between bot
  * and MongoDB. This will reduce the amount of DB constructive
  * calls outside of DB related locations.
  */
-export function fetchUserData(): void {
-	console.log("NYI - Fetch User Data");
+export async function updateGuildData(guildId: Snowflake, validChannels: string[]): Promise<IDAOResult> {
+	return new Promise(resolve => {
+		fetchGuildData(guildId)
+			.then(res => {
+				const guildData = res.guildData;
+
+				if (guildData) {
+					guildData.validChannels = validChannels;
+
+					guildDataModel
+						.updateOne({ guildId }, { validChannels })
+						.then(res => {
+							if (res.n === 0) {
+								resolve({ result: false, message: "No guildData exists." });
+							} else if (res.nModified === 0) {
+								resolve({ result: false, message: "Failed to update guildData." });
+							} else {
+								resolve({
+									result: true,
+									message: "Successfully updated guildData.",
+									guildData,
+								});
+							}
+						})
+						.catch((err: Error) => resolve({ result: false, message: err.message }));
+				} else {
+					resolve({ result: false, message: "No guildData exists." });
+				}
+			})
+			.catch((err: Error) => resolve({ result: false, message: err.message }));
+	});
 }
 
 /**
- * updateGuildData is inteded to be an intermediary between bot
+ * updateUserData is intended to be an intermediary between bot
  * and MongoDB. This will reduce the amount of DB constructive
  * calls outside of DB related locations.
  */
-export function updateGuildData(): void {
-	console.log("NYI - Update Guild Data");
+export async function updateUserData(
+	guildId: Snowflake,
+	userId: Snowflake,
+	channelId: Snowflake,
+	reputation: number
+): Promise<IDAOResult> {
+	return new Promise(resolve => {
+		fetchGuildData(guildId)
+			.then(res => {
+				const guildData = res.guildData;
+
+				if (guildData) {
+					const userData = guildData.userData;
+					const userRepData = userData.find(userDataObject => userDataObject.userId === userId);
+
+					if (userRepData) {
+						const hasChannelData = userRepData.reputation.find(
+							channelObject => channelObject.channelId === channelId
+						);
+
+						if (hasChannelData) {
+							const newReputation: IChannelData = {
+								channelId,
+								reputation,
+							};
+
+							const newUserRepData = userRepData.reputation.filter(
+								channelObject => channelObject.channelId != channelId
+							);
+							newUserRepData.push(newReputation);
+
+							const newUserData = {
+								userId,
+								reputation: newUserRepData,
+							};
+
+							const newGuildUserData = userData.filter(userDataObject => userDataObject.userId != userId);
+							newGuildUserData.push(newUserData);
+
+							guildData.userData = newGuildUserData;
+
+							guildDataModel
+								.updateOne({ guildId }, { userData: newGuildUserData })
+								.then(() =>
+									resolve({
+										result: true,
+										message: "Successfully updated userData.",
+										guildData,
+										userData: newUserData,
+									})
+								)
+								.catch((err: Error) => resolve({ result: false, message: err.message }));
+						} else {
+							resolve({
+								result: false,
+								message: "No channelData exists.",
+								guildData,
+								userData: userRepData,
+							});
+						}
+					} else {
+						resolve({ result: false, message: "No userData exists.", guildData });
+					}
+				} else {
+					resolve({ result: false, message: "No guildData exists." });
+				}
+			})
+			.catch((err: Error) => resolve({ result: false, message: err.message }));
+	});
 }
 
 /**
- * updateUserData is inteded to be an intermediary between bot
+ * deleteGuildData is intended to be an intermediary between bot
  * and MongoDB. This will reduce the amount of DB constructive
  * calls outside of DB related locations.
  */
-export function updateUserData(): void {
-	console.log("NYI - Update User Data");
+export async function deleteGuildData(guildId: Snowflake): Promise<IDAOResult> {
+	return new Promise(resolve => {
+		fetchGuildData(guildId)
+			.then(guildData => {
+				if (guildData.guildData) {
+					guildDataModel
+						.deleteOne({ guildId })
+						.then(() => resolve({ result: true, message: "Successfully deleted guildData." }))
+						.catch((err: Error) => resolve({ result: false, message: err.message }));
+				} else {
+					resolve({ result: false, message: "No guildData exists." });
+				}
+			})
+			.catch((err: Error) => resolve({ result: false, message: err.message }));
+	});
+}
+
+/**
+ * deleteUserData is intended to be an intermediary between bot
+ * and MongoDB. This will reduce the amount of DB constructive
+ * calls outside of DB related locations.
+ */
+export async function deleteUserData(guildId: Snowflake, userId: Snowflake): Promise<IDAOResult> {
+	return new Promise(resolve => {
+		fetchGuildData(guildId)
+			.then(fetchedGuildData => {
+				const guildData = fetchedGuildData.guildData;
+				if (guildData) {
+					const userData = guildData.userData;
+					const savedUserData = userData.find(userDataObject => userDataObject.userId === userId);
+
+					if (savedUserData) {
+						const newUserData = userData.filter(userDataObject => userDataObject.userId != userId);
+
+						guildData.userData = newUserData;
+
+						guildDataModel
+							.updateOne({ guildId }, { userData: newUserData })
+							.then(() => resolve({ result: true, message: "Successfully deleted userData.", guildData }))
+							.catch((err: Error) => resolve({ result: false, message: err.message }));
+					} else {
+						resolve({ result: false, message: "No userData exists.", guildData });
+					}
+				} else {
+					resolve({ result: false, message: "No guildData exists." });
+				}
+			})
+			.catch((err: Error) => resolve({ result: false, message: err.message }));
+	});
 }
