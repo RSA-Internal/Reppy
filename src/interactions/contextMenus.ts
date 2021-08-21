f6654195b3df6d98f7352aec0922b307bbf69782
Chris <chris.barwick16@gmail.com>
2021-08-20 18:16:31 -0700

    Implement functionality of contextConvertToAnswer

--- src/interactions/contextMenus.ts
+++ src/interactions/contextMenus.ts
@@ -1,8 +1,79 @@
-import type { ContextMenuInteraction, Message } from "discord.js";
+import { ContextMenuInteraction, Message, MessageActionRow, MessageButton, MessageEmbed } from "discord.js";
 import { fetchGuildData } from "../daos/GuildDataDAO";
 
-export function contextConvertToAnswer(interaction: ContextMenuInteraction): void {
-	interaction.reply({ ephemeral: true, content: "NYI - contextConvertToAnswer" }).catch(console.error.bind(console));
+export async function contextConvertToAnswer(interaction: ContextMenuInteraction): Promise<void> {
+	await interaction.deferReply({ ephemeral: true });
+	const guild = interaction.guild;
+
+	if (guild) {
+		const fetchResult = await fetchGuildData(guild.id);
+		const guildData = fetchResult.guildData;
+
+		if (guildData) {
+			const message = interaction.options["_hoistedOptions"][0].message as Message;
+
+			if (message) {
+				const channel = message.channel;
+
+				if (channel && channel.isThread()) {
+					const parent = channel.parentId;
+
+					if (parent && guildData.validChannels.includes(parent)) {
+						const author = message.author.id;
+
+						if (interaction.user.id === author) {
+							// convert to answer.
+							if (message.embeds.length > 0) {
+								await interaction.editReply("This message is already an answer.");
+							} else {
+								const content = message.content;
+								const member = await guild.members.fetch(author);
+
+								await channel.send({
+									embeds: [
+										new MessageEmbed()
+											.setTitle("Answer")
+											.setAuthor(member.displayName)
+											.addField("\u200b", content, true)
+											.setFooter("Upvotes: 0 | Downvotes: 0"),
+									],
+									components: [
+										new MessageActionRow().addComponents([
+											new MessageButton()
+												.setCustomId("upvote")
+												.setLabel("Upvote")
+												.setStyle("PRIMARY"),
+											new MessageButton()
+												.setCustomId("downvote")
+												.setLabel("Downvote")
+												.setStyle("DANGER"),
+										]),
+									],
+								});
+
+								await message.delete();
+								await interaction.editReply("Message successfully converted to an answer.");
+							}
+						} else {
+							await interaction.editReply(
+								"You are not allowed to convert another member's message to a answer."
+							);
+						}
+					} else {
+						await interaction.editReply("This channel is not under valid reputation gainable channel.");
+					}
+				} else {
+					await interaction.editReply("This is not a valid location to convert a message to an answer.");
+				}
+			}
+		} else {
+			await interaction.editReply(fetchResult.message);
+		}
+	} else {
+		await interaction.editReply("Failed to fetch guildId from interaction.");
+	}
+
+	return;
 }
 
 export async function contextConvertToQuestion(interaction: ContextMenuInteraction): Promise<void> {
@@ -59,10 +130,10 @@ export async function contextConvertToQuestion(interaction: ContextMenuInteracti
 	return;
 }
 
-export function contextAcceptAnswer(interaction: ContextMenuInteraction): void {
-	interaction.reply({ ephemeral: true, content: "NYI - contextAcceptAnswer" }).catch(console.error.bind(console));
+export async function contextAcceptAnswer(interaction: ContextMenuInteraction): Promise<void> {
+	await interaction.reply({ ephemeral: true, content: "NYI - contextAcceptAnswer" });
 }
 
-export function contextFlag(interaction: ContextMenuInteraction): void {
-	interaction.reply({ ephemeral: true, content: "NYI - contextFlag" }).catch(console.error.bind(console));
+export async function contextFlag(interaction: ContextMenuInteraction): Promise<void> {
+	await interaction.reply({ ephemeral: true, content: "NYI - contextFlag" });
 }
